cfce8e8a63a285a339e182ff11544948c1bb2c9d
Shubham Salunkhe <82544281+shubhamsalunkhe16@users.noreply.github.com>
2021-09-17 16:29:18 +0530

    Update UserController.java

--- GoPhygitalTestProject2.0/src/main/java/io/gophygital/test/controller/UserController.java
+++ GoPhygitalTestProject2.0/src/main/java/io/gophygital/test/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,8 +46,7 @@ public class UserController {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@PostMapping("/")
-	public User registerUser(@RequestBody User user) throws Exception {
-
+	public ResponseEntity<?> registerUser(@RequestBody User user) throws Exception {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
 		Role role = new Role();
@@ -59,9 +60,14 @@ public class UserController {
 		Set<UserRole> userRoleSet = new HashSet<>();
 		userRoleSet.add(userRole);
 
-		User userRegister = this.userService.registerUser(user, userRoleSet);
+		try {
+			User userRegister = this.userService.registerUser(user, userRoleSet);
 
-		return userRegister;
+			return new ResponseEntity<>(userRegister, HttpStatus.CREATED);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>("UserAlreadyFoundException", HttpStatus.BAD_REQUEST);
+		}
 
 	}
 
